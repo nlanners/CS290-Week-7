f687eb8b60ec446ea269b164353d582a012415a7
nlanners <lannersn@oregonstate.edu>
2021-02-19 19:49:14 -0700

    started get response

---
+++ get-post-checker.js
@@ -0,0 +1,34 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 65535);
+
+app.get('/', function(rew,res){
+    var queryParams = [];
+    for (var p in req.query){
+        queryParams.push({'name': p, 'value':req.query[p]})
+    }
+    var context = {};
+    context.dataList = queryParams;
+    res.render('get-response', context);
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
