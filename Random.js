b04a4c5553658285f5120b9f1c66daf9a73fba44
nlanners <lannersn@oregonstate.edu>
2021-02-19 18:03:38 -0700

    added Random.js

---
+++ Random.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', 65535);
+
+app.get('/', function(req, res){
+    res.type('text/plain');
+    res.send(Math.random());
+});
+
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
